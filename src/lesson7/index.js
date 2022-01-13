b235481c2f08943d08957092f18d6311235e5c01
david dimon <david_dimon@hotmail.com>
2022-01-13 18:00:38 -0300

    feat: added more tests

--- src/lesson7/index.js
+++ src/lesson7/index.js
@@ -1,3 +1,55 @@
+/**
+ * Criar um algoritmo para somar dois valores
+ * inputs ->
+ * 1,1 = 2
+ * 3,1 = 4
+ * -1,0 = -1
+ */
 export const sum = (value1, value2) => {
-  return value1 + value2
+  return null
+}
+
+/**
+ * Criar um algoritmo para subtrair dois valores
+ * inputs ->
+ * 1,1 = 0
+ * 3,1 = 2
+ * -1,0 = -1
+ */
+export const subtract = (value1, value2) => {
+  return null
+}
+
+/**
+ * Criar um algoritmo para descobrir se o usuário é menor de idade
+ * se for menor de idade, retornar true, se for maior de idade, retornar false
+ * inputs ->
+ * 18 = true
+ * 19 = true
+ * 3 = false
+ */
+export const isUnderAge = (age) => {
+  return null
+}
+
+/**
+ * Criar um algoritmo para descobrir a idade do usuário, apenas passando o ano de nascimento.
+ * inputs ->
+ * 1995 = 27
+ * 2000 = 22
+ * 2021 = 1
+ */
+export const getAgeByBirthYear = (birthYear) => {
+  return null
+}
+
+/**
+ * Criar um algoritmo para achar a sequencia de fibonacci
+ * inputs ->
+ * 1 = 1
+ * 5 = 5
+ * 8 = 21
+ */
+export const fibonacci = (value) => {
+  return null
 }
--- src/lesson7/index.test.js
+++ src/lesson7/index.test.js
@@ -1,7 +1,71 @@
-import { sum } from './'
+import { sum, subtract, isUnderAge, getAgeByBirthYear, fibonacci } from './'
 
 describe('test sum', () => {
   it('test 1', () => {
     expect(sum(1, 1)).toEqual(2)
+  })
+
+  it('test 2', () => {
+    expect(sum(3, 1)).toEqual(4)
+  })
+
+  it('test 3', () => {
+    expect(sum(-1, 0)).toEqual(-1)
+  })
+})
+
+describe('test subtract', () => {
+  it('test 1', () => {
+    expect(subtract(1, 1)).toEqual(0)
+  })
+
+  it('test 2', () => {
+    expect(subtract(3, 1)).toEqual(2)
+  })
+
+  it('test 3', () => {
+    expect(subtract(-1, 0)).toEqual(-1)
+  })
+})
+
+describe('test isUnderAge', () => {
+  it('test 1', () => {
+    expect(isUnderAge(18)).toBeTruthy()
+  })
+
+  it('test 2', () => {
+    expect(isUnderAge(19)).toBeTruthy()
+  })
+
+  it('test 3', () => {
+    expect(isUnderAge(3)).toBeFalsy()
+  })
+})
+
+describe('test getAgeByBirthYear', () => {
+  it('test 1', () => {
+    expect(getAgeByBirthYear(1995)).toEqual(27)
+  })
+
+  it('test 2', () => {
+    expect(getAgeByBirthYear(2000)).toEqual(22)
+  })
+
+  it('test 3', () => {
+    expect(getAgeByBirthYear(2021)).toEqual(1)
+  })
+})
+
+describe('test fibonacci', () => {
+  it('test 1', () => {
+    expect(fibonacci(1)).toEqual(1)
+  })
+
+  it('test 2', () => {
+    expect(fibonacci(5)).toEqual(5)
+  })
+
+  it('test 3', () => {
+    expect(fibonacci(8)).toEqual(21)
   })
 })
